69358c7d9208643c11bc5f513e164b36fef77cf2
terminatorDX <jain.cj.chetan@gmail.com>
2020-03-28 11:58:36 +0530

    some chnages for creating new routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+    res.render("index", { title: "Express" });
 });
+router.get("/about", (req, res, next) => {
+    res.render("about", { title: "abou page of codeshare" });
+});
+router
+    .route("/contact")
+    .get((req, res, next) => {
+        res.render("contact", { title: "contact page of codeshare" });
+    })
+    .post((req, res, next) => {
+        req.checkBody("name", "Empty Name").notEmpty();
+        req.checkBody("email", "Invalid Email").notEmpty();
+        req.checkBody("message", "Empty Message").notEmpty();
+        var errors = req.validationErrors();
+        if (errors) {
+            res.render("contact", {
+                title: "contact page of codeshare",
+                name: req.body.name,
+                email: req.body.email,
+                message: req.body.message,
+                errorMessages: errors
+            });
+        }
+
+        res.render("thank", { title: "post of contact" });
+    });
 
 module.exports = router;
