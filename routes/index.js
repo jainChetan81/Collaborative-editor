4604a048dc1f9b1408f36cb6ea16f323c4fef450
terminatorDX <jain.cj.chetan@gmail.com>
2020-03-28 19:53:41 +0530

    fixed errors of nodemailer

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const nodemailer = require("nodemailer"),
     express = require("express"),
     router = express.Router(),
-    config = require("../config"),
-    transporter = nodemailer.createTransport(config.mailer);
+    config = require("../config");
 /* GET home page. */
 router.get("/", function(req, res, next) {
     res.render("index", { title: "Express" });
@@ -28,20 +27,22 @@ router
                 message: req.body.message,
                 errorMessages: errors
             });
+        } else {
+            var mailOptions = {
+                from: "chetan jain <no-reply@gmail.com>",
+                to: "dangerxkills@gmail.com",
+                subject: "you got a new message from visitor",
+                text: req.body.message
+            };
+            const transporter = nodemailer.createTransport(config.mailer);
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return console.log("error in sending mail : ", error);
+                }
+                res.render("thank", { title: "post of contact" });
+            });
         }
         //todo : if no errors are found
-        var mailOptions = {
-            from: "chetan jain <no-reply@gmail.com>",
-            to: "dangerxkills@gmail.com",
-            subject: "you got a new message from visitor",
-            text: req.body.message
-        };
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log("error in sending mail : ", error);
-            }
-            res.render("thank", { title: "post of contact" });
-        });
     });
 
 module.exports = router;
